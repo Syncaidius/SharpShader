7800a81ce92b367ae2a86c260adcb4dc53027a7c
James Yarwood <syncaidius@googlemail.com>
2018-11-28 12:24:54 +0000

    Better handling of leading and trailing code comments.

--- SharpShader/Processors/NamespaceProcessor.cs
+++ SharpShader/Processors/NamespaceProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
@@ -25,13 +26,29 @@ namespace SharpShader
                 if (n == syntax.CloseBraceToken || n == syntax.OpenBraceToken)
                     continue;
 
-                childSource += $"{n.GetLeadingTrivia()}{n.ToString()}{n.GetTrailingTrivia()}{Environment.NewLine}";
+                string leadingTrivia = ProcessTrivia(n.GetLeadingTrivia());
+                leadingTrivia += !string.IsNullOrWhiteSpace(leadingTrivia) ? Environment.NewLine : "";
+
+                string trailingTrivia = ProcessTrivia(n.GetTrailingTrivia());
+                childSource += $"{leadingTrivia}{n.ToString()}{trailingTrivia}{Environment.NewLine}";
 
                 if (n != last)
                     childSource += Environment.NewLine;
             }
 
             source.Replace(syntax.ToString(), childSource, syntax.SpanStart, syntax.Span.Length);
+        }
+
+        private static string ProcessTrivia(SyntaxTriviaList trivia)
+        {
+            string result = "";
+            foreach(SyntaxTrivia t in trivia)
+            {
+                if (t.IsKind(SyntaxKind.SingleLineCommentTrivia) || t.IsKind(SyntaxKind.MultiLineCommentTrivia))
+                    result += t.ToString();
+            }
+
+            return result;
         }
     }
 }
