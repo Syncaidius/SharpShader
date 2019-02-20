e537856fba23ffc2652ae13680924a3dbe308a18
James Yarwood <syncaidius@googlemail.com>
2019-02-20 16:09:52 +0000

    ShaderMember now contains resource type information. Added ShaderMember.ResourceType and .ResourcebaseType properties.

--- SharpShader/Result/ShaderMember.cs
+++ SharpShader/Result/ShaderMember.cs
@@ -11,27 +11,37 @@ namespace SharpShader.Result
         /// <summary>
         /// Gets information about an individual element within the current <see cref="ShaderMember"/>.
         /// </summary>
-        public ShaderElementInfo ElementInfo { get; internal set; }
+        public ShaderElementInfo ElementInfo { get; private set; }
 
         /// <summary>
         /// Gets the total size of the member, in bytes.
         /// </summary>
-        public int SizeOf { get; internal set; }
+        public int SizeOf { get; private set; }
 
         /// <summary>
         /// Gets the identifier or name of the element.
         /// </summary>
-        public string Name { get; internal set; }
+        public string Name { get; private set; }
 
         /// <summary>
         /// Gets the offset of the expected value, from the start of it's parent type (e.g. struct, buffer, etc), in bytes.
         /// </summary>
-        public int StartOffset { get; internal set; }
+        public int StartOffset { get; private set; }
 
         /// <summary>
         /// Gets the array size of the current <see cref="ShaderMember"/>. If the member is not an array, this list will be empty.
         /// </summary>
-        public IReadOnlyList<int> ArrayDimensions { get; internal set; }
+        public IReadOnlyList<int> ArrayDimensions { get; private set; }
+
+        /// <summary>
+        /// Gets the resource type of the current <see cref="ShaderMember"/>. <see cref="ShaderResourceType.Unknown"/> is returned if the member is not a resource binding.
+        /// </summary>
+        public ShaderResourceType ResourceType { get; private set; }
+
+        /// <summary>
+        /// Gets the base resource type of the current <see cref="ShaderMember"/>. <see cref="ShaderResourceBaseType.Unknown"/> is returned if the member is not a resource binding.
+        /// </summary>
+        public ShaderResourceBaseType ResourceBaseType { get; private set; }
 
         /// <summary>
         /// Gets the number of values expected by the current <see cref="ShaderMember"/>. This may be for example, the length of an array, or the number of components in a vector.
@@ -44,7 +54,7 @@ namespace SharpShader.Result
             ArrayDimensions = new List<int>(mField.ArrayDimensions).AsReadOnly();
 
             ElementCount = mField.GetTotalArrayElements();
-            StartOffset = mField.PackOffsetBytes.Value;
+            StartOffset = mField.PackOffsetBytes.HasValue ? mField.PackOffsetBytes.Value : 0;
             ElementInfo = new ShaderElementInfo()
             {
                 DataType = mField.Type.DataType,
@@ -56,6 +66,8 @@ namespace SharpShader.Result
             };
             SizeOf = mField.GetTotalSizeOf();
             Name = mField.Name;
+            ResourceType = mField.ResourceType;
+            ResourceBaseType = mField.ResourceBaseType;
         }
     }
 }
